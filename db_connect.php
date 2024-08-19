71c9b2ffcef14c54b119f1607f29d4501f8eb451
anna-gie <angelikaanna.ludzik@gmail.com>
2024-08-19 11:13:46 +0200

    adding new files

---
+++ db_connect.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('error_reporting', E_ALL);
+
+$hostname = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "";
+
+
+
+$connect = new mysqli($hostname, $username, $password, $dbname);
+
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+function cleanInput($input)
+{
+    $data = trim($input);
+    $data = strip_tags($data);
+    $data = htmlspecialchars($data);
+
+    return $data;
+}
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+// ob_start();
+// session_start();
+
+// if (!isset($_SESSION["user"]) && !isset($_SESSION["admin"])) {
+//     header("Location: login.php");
+//     exit();
+// }
+
+
+require_once  "db_connect.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include 'components/navbar.php';
+    ?>
+    <!-- Hero goes here -->
+
+
+    <!-- Index File goes here -->
+
+
+
+
+    <!-- Footer start -->
+    <?php include 'components/footer.php';
+    ?>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
