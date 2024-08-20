fdc08f8484151316db65e7885e5072fc583e5bc0
anna-gie <angelikaanna.ludzik@gmail.com>
2024-08-20 13:32:23 +0200

    button with link

--- components/hero.php
+++ components/hero.php
@@ -6,7 +6,7 @@
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Offers for every taste</h5>
                     <p>Save up to 50% on seleced products. As long as stock is available. </p>
-                    <button class="btn btn-outline-light" type="button">Explore now</button>
+                    <a href="../products/product.php" class="btn btn-outline-light" type="button">Explore now</a>
                 </div>
             </div>
             <div class="carousel-item">
@@ -14,7 +14,7 @@
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Cold cuts</h5>
                     <p>No Artificial Additives | 100% Satisfaction Guaranteed</p>
-                    <button class="btn btn-outline-light" type="button">Discover now</button>
+                    <a href="../products/product.php" class="btn btn-outline-light" type="button">Discover now</a>
                 </div>
             </div>
             <div class="carousel-item">
@@ -22,7 +22,7 @@
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Whole Grains</h5>
                     <p>100% Whole grains goodness for a healthier more nutrious choice</p>
-                    <button class="btn btn-outline-light" type="button">Shop now</button>
+                    <a href="../products/product.php" class="btn btn-outline-light" type="button">Shop now</a>
                 </div>
             </div>
         </div>
@@ -37,22 +37,22 @@
     </div>
 </div>
 
-    <!-- Content Sections -->
-    <div class="container py-4 content-section">
-        <div class="row align-items-md-stretch">
-            <div class="col-md-6">
-                <div class="h-100 p-5 text-bg-dark">
-                    <h2>Our Specialties</h2>
-                    <p>Explore our unique selection of tech-themed delicacies and gourmet treats.</p>
-                    <button class="btn btn-outline-light" type="button">Discover More</button>
-                </div>
+<!-- Content Sections -->
+<div class="container py-4 content-section">
+    <div class="row align-items-md-stretch">
+        <div class="col-md-6">
+            <div class="h-100 p-5 text-bg-dark">
+                <h2>Our Specialties</h2>
+                <p>Explore our unique selection of tech-themed delicacies and gourmet treats.</p>
+                <button class="btn btn-outline-light" type="button">Discover More</button>
             </div>
-            <div class="col-md-6">
-                <div class="h-100 p-5 bg-body-tertiary border">
-                    <h2>Customer Favorites</h2>
-                    <p>Check out our most loved products by our loyal customers.</p>
-                    <button class="btn btn-outline-light" type="button">Shop Now</button>
-                </div>
+        </div>
+        <div class="col-md-6">
+            <div class="h-100 p-5 bg-body-tertiary border">
+                <h2>Customer Favorites</h2>
+                <p>Check out our most loved products by our loyal customers.</p>
+                <button class="btn btn-outline-light" type="button">Shop Now</button>
             </div>
         </div>
     </div>
+</div>
