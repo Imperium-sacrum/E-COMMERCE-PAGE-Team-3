f166f15343d7769eae9ab73074a347d99e41cdeb
anna-gie <angelikaanna.ludzik@gmail.com>
2024-08-20 15:12:01 +0200

    cards.php

--- cards.php
+++ cards.php
@@ -2,14 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-ob_start();
-session_start();
-if (!isset($_SESSION["user"]) && !isset($_SESSION["admin"])) {
-    header("Location: login.php");
-    exit();
-}
+// ob_start();
+// session_start();
+// if (!isset($_SESSION["user"]) && !isset($_SESSION["admin"])) {
+//     header("Location: login.php");
+//     exit();
+// }
 
-require_once "../db_components/db_connect.php";
+require_once "db_components/db_connect.php";
 
 $categorySql = "SELECT * FROM `product_categories`";
 $categoryResult = mysqli_query($connect, $categorySql);
@@ -22,11 +22,11 @@ $result = mysqli_query($connect, $sql);
 $cards = "";
 
 if (mysqli_num_rows($result) == 0) {
-    $cards = "<p>No products found</p>";
+  $cards = "<p>No products found</p>";
 } else {
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    foreach ($rows as $key => $row) {
-        $cards .= "<div>
+  $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  foreach ($rows as $key => $row) {
+    $cards .= "<div>
        <div class='card my-3'>
   <nav>
     <a href='../index.php'><svg class='arrow' version='1.1' viewBox='0 0 512 512' width='512px' xml:space='preserve' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><polygon points='352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 ' stroke='#727272'/></svg></a>
@@ -35,7 +35,7 @@ if (mysqli_num_rows($result) == 0) {
   </nav>
   <div class='d-flex'>
   <div class='photo'>
-    <img src='../images/{$row["image"]}'>
+    <img src='images/{$row["image"]}'>
   </div>
 
   <div class='description'>
@@ -53,7 +53,7 @@ if (mysqli_num_rows($result) == 0) {
        
            
          ";
-    }
+  }
 }
 
 
@@ -64,26 +64,26 @@ mysqli_close($connect);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../styles/cards.css">
-    <link rel="stylesheet" href="../styles/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Products</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="styles/cards.css">
+  <link rel="stylesheet" href="styles/style.css">
 </head>
 <a href=""></a>
 
 <body id=cards-body>
-    <?php include '../components/navbar.php';
-    ?>
+  <?php include 'components/navbar.php';
+  ?>
 
-    <div class="section-card container">
-        <div class="row row-cols-lg-2 row-cols-md-2 row-cols-sm-1 row-cols-xs-1">
-            <?= $cards ?>
-        </div>
+  <div class="section-card container">
+    <div class="row row-cols-lg-2 row-cols-md-2 row-cols-sm-1 row-cols-xs-1">
+      <?= $cards ?>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
