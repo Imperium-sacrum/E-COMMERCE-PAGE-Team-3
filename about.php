fac72d7104b6736514986099b1c59c3cbf77e54d
anna-gie <angelikaanna.ludzik@gmail.com>
2024-08-21 14:36:00 +0200

    about.php

--- about.php
+++ about.php
@@ -7,6 +7,8 @@
     <title>About Us</title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/about.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
 </head>
 
@@ -16,13 +18,14 @@
     </div>
     <div class="content-about container">
         <h1>TO KNOW LIST</h1>
+        <hr>
         <ul>
             <h2>
                 <li> About Us</li>
                 <h3>We're passionate about bringing high-quality, delicious food directly to your doorstep. As a small, family-run business, we believe in the power of good food to bring people together and create lasting memories.</h3>
             </h2>
             <h2>
-                <li> Our Story</li>
+                <li class="mt-2"> Our Story</li>
 
                 <div class="bg">
 
@@ -44,15 +47,6 @@
                 </div>
             </h2>
             <h2>
-                <li> Our Products</li>
-                <h3>Our catalog features a wide range of [types of food], including:
-
-                    [Product Category 1]: [Brief description]
-                    [Product Category 2]: [Brief description]
-                    [Product Category 3]: [Brief description]
-                    Whether you're preparing a simple meal for your family or hosting a dinner party, we have something to inspire your next culinary creation.</h3>
-            </h2>
-            <h2>
                 <li> Why Choose Us?</li>
                 <h3>Quality You Can Trust: We only offer products that meet our high standards. If we wouldn’t serve it at our own table, we won’t sell it.
                     Personalized Service: As a small business, we pride ourselves on our personal touch. We treat every customer like family.
@@ -61,8 +55,17 @@
                 </h3>
             </h2>
             <h2>
+                <hr>
                 <li> Get in Touch</li>
-            </h2>
+                <ul class="get">
+                    <a href="contact.php">Contact Us</a>
+                    <a href="#">Chat with us</a>
+                    <a href="#show">Visit us</a>
+                    <h5 id="show">Our Team E-commerce <br>
+                        addresse </h5>
+                </ul>
+        </ul>
+        </h2>
 
         </ul>
 
