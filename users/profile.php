a1b87ea126879ef2be5602432ddff297faf1c981
Kutayaltan <kutay.altan1992@gmail.com>
2024-08-28 13:56:32 +0200

    sg

--- users/profile.php
+++ users/profile.php
@@ -71,10 +71,10 @@ $row = mysqli_fetch_assoc($result);
                                             <h6>User Name</h6>
                                             <p class="text-muted"><?= $row["username"] ?></p>
                                         </div>
-                                        <a class="btn btn-outline-dark m-1" href='../user_dashboard.php?id=<?= $row["user_id"] ?>'>Orders</a>
-                                        <a href="profile-edit.php" class="btn btn-outline-dark m-1">Edit profile</a>
-                                        <a href="../chat/chat.php" class="btn btn-outline-dark m-1">Chat</a>
-                                        <a href='delete.php?id=<?= $row["user_id"] ?>' class='btn btn-danger m-1'>Delete</a>
+                                        <a class="btn btn-outline-dark m-1 btn-sm" href='../user_dashboard.php?id=<?= $row["user_id"] ?>'>Orders</a>
+                                        <a href="profile-edit.php" class="btn btn-outline-dark m-1 btn-sm">Edit profile</a>
+                                        <a href="../chat/chat.php" class="btn btn-outline-dark m-1 btn-sm">Chat</a>
+                                        <a href='delete.php?id=<?= $row["user_id"] ?>' class='btn btn-danger m-1 btn-sm'>Delete</a>
                                     </div>
                                 </div>
                             </div>
